b9ee84ba5d6b5c8b6aba00df30a5da2ec3785543
ksss <co000ri@gmail.com>
2016-04-30 21:59:37 +0900

    Return or yield when just found

--- lib/active_tsv/relation.rb
+++ lib/active_tsv/relation.rb
@@ -24,21 +24,18 @@ module ActiveTsv
     def first
       keys = @table.keys
       key_to_value_index = keys.each_with_index.map { |k, index| [k, index] }.to_h
-      value = @table.open { |csv|
+      @table.open do |csv|
         csv.gets
-        csv.find do |value|
-          @conditions.all? do |cond|
+        csv.each do |value|
+          return @table.new(@table.keys.zip(value).to_h) if @conditions.all? { |cond|
             cond.values.all? do |k, v|
               value[key_to_value_index[k]].__send__(cond.method_name, v.to_s)
             end
-          end
+          }
         end
-      }
-      if value
-        @table.new(@table.keys.zip(value).to_h)
-      else
-        nil
       end
+
+      nil
     end
 
     def last
@@ -50,18 +47,15 @@ module ActiveTsv
 
       keys = @table.keys
       key_to_value_index = keys.each_with_index.map { |k, index| [k, index] }.to_h
-      values = @table.open { |csv|
+      @table.open do |csv|
         csv.gets
-        csv.select do |value|
-          @conditions.all? do |cond|
+        csv.each do |value|
+          yield @table.new(@table.keys.zip(value).to_h) if @conditions.all? { |cond|
             cond.values.all? do |k, v|
               value[key_to_value_index[k]].__send__(cond.method_name, v.to_s)
             end
-          end
+          }
         end
-      }
-      values.each do |value|
-        yield @table.new(@table.keys.zip(value).to_h)
       end
     end
   end
