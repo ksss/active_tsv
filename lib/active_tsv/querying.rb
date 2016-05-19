34bc8311ceb4eec46470515141b72c2d3a69a72b
ksss <co000ri@gmail.com>
2016-05-19 10:56:59 +0900

    Defined line should plus one

--- lib/active_tsv/querying.rb
+++ lib/active_tsv/querying.rb
@@ -2,7 +2,7 @@ module ActiveTsv
   module Querying
     METHODS = %i(first last take where count order group pluck minimum maximum)
     METHODS.each do |m|
-      module_eval <<-DEFINE_METHOD, __FILE__, __LINE__
+      module_eval <<-DEFINE_METHOD, __FILE__, __LINE__ + 1
         def #{m}(*args, &block)
           all.#{m}(*args, &block)
         end
